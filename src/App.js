353cd2d2aa3d8fdea6cc728e275ba63c27e8343e
Ahmed Younas <142696963+4hmed7ounas@users.noreply.github.com>
2024-05-19 03:56:01 +0500

    contact-form

--- src/App.js
+++ src/App.js
@@ -4,11 +4,10 @@ import { useState } from "react";
 function App() {
   const [name, setName] = useState("");
   const [message, setMessage] = useState("");
-  console.log(name, message);
 
   return (
     <div>
-      <form name="contact" method="POST">
+      <form name="contact" method="POST" data-netlify="true" netlify>
         <input type="hidden" name="form-name" value="contact" />
         <div>
           <label htmlFor="name">Name</label>
@@ -20,18 +19,18 @@ function App() {
             value={name}
             onChange={(e) => setName(e.target.value)}
           />
-          <div>
-            <label htmlFor="message">Message</label>
-            <textarea
-              name="message"
-              id="message"
-              placeholder="your message"
-              value={message}
-              onChange={(e) => setMessage(e.target.value)}
-            ></textarea>
-          </div>
-          <button type="submit">Submit</button>
         </div>
+        <div>
+          <label htmlFor="message">Message</label>
+          <textarea
+            name="message"
+            id="message"
+            placeholder="your message"
+            value={message}
+            onChange={(e) => setMessage(e.target.value)}
+          ></textarea>
+        </div>
+        <button type="submit">Submit</button>
       </form>
     </div>
   );
